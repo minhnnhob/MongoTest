125f673fad8bd610b332e0207aeb27659174b208
Minh <86398860+minhnnhob@users.noreply.github.com>
2023-02-21 08:54:23 +0700

    create view

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-var express = require('express')
+var express = require('express');
+
 var app = express()
 
 app.set('view engine','hbs')
 app.use(express.urlencoded({extended:true}))
 
-app.get('/',(req,res)=>{
-    res.render('main')
+var url = 'mongodb://127.0.0.1:27017';
+var MongoClient = require('mongodb').MongoClient;
+
+app.get('/',async(req,res)=>{
+    let client = await MongoClient.connect(url)// doi cau lenh nay chay xong moi chay cac cau sau
+    let dbo = client.db("GCH1005")
+    let products = await dbo.collection("products").find().toArray()
+    res.render('main',{'products':products})
 })
 
-const PORT =    process.env.PORT || 8000
+const PORT =  process.env.PORT || 8000
 app.listen(PORT,()=>{
     console.log("okehehe")
 })
