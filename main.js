7aa0c018cc3f32d28bd1ac2e95cf956574311f2a
Minh <86398860+minhnnhob@users.noreply.github.com>
2023-02-28 08:47:55 +0700

    KOOK

--- main.js
+++ main.js
@@ -6,13 +6,20 @@ var app = express()
 app.set('view engine','hbs')
 app.use(express.urlencoded({extended:true}))
 
-var url = 'mongodb://127.0.0.1:27017';
+var url = 'mongodb+srv://Minh123:Minh123@cluster0.qyevixf.mongodb.net/test';
 var MongoClient = require('mongodb').MongoClient;
 
 app.post('/add',async (req,res)=>{
     const name = req.body.txtName
     const price = req.body.txtPrice
     const picURL = req.body.picURL
+
+    if(name.length <= 5){
+        res.render('add',{name_err: 'it chu the, danh it nhat 5 chu vao'})
+        return
+    }
+
+
     const newProduct = {
         'name' : name,
         'price' : price,
@@ -33,6 +40,27 @@ app.get('/delete/:id',async (req,res)=>{
     let client = await MongoClient.connect(url)
     let dbo = client.db("GCH1005")
     await dbo.collection("products").deleteOne({"_id":new ObjectId(id)})
+    res.redirect("/")
+})
+
+app.get('/edit/:id', async (req,res)=>{
+    const id = req.params.id
+    let client = await MongoClient.connect(url)
+    let dbo = client.db("GCH1005")
+    const prod= await dbo.collection("products").findOne({"_id":new ObjectId(id)})
+    res.render('edit',{prod:prod})
+})
+
+app.post('/edit',async (req,res)=>{
+    const id = req.body.id
+    const name = req.body.txtName
+    const price = req.body.txtPrice
+    const picURL = req.body.picURL
+    let newValues ={ $set : {name: name,price:price, picURL:picURL}};
+
+    let client = await MongoClient.connect(url)
+    let dbo = client.db("GCH1005")
+    await dbo.collection("products").updateOne({"_id":new ObjectId(id)},newValues)
     res.redirect("/")
 })
 
